7ff5a2a176cd2321dac88bffafa2931639c22608
Vutha Phai <vuthaphai@Vuthas-MacBook-Pro.local>
2024-10-20 17:26:45 +0700

    wip

--- src/main/java/guru/springframework/spring_6_resttemplate/model/BeerDTOPageDeserializer.java
+++ src/main/java/guru/springframework/spring_6_resttemplate/model/BeerDTOPageDeserializer.java
@@ -8,7 +8,6 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.data.domain.PageRequest;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -20,35 +19,31 @@ public class BeerDTOPageDeserializer extends JsonDeserializer<BeerDTOPageImpl<Be
             throws IOException, JsonProcessingException {
 
         ObjectMapper mapper = (ObjectMapper) p.getCodec();
-        mapper.registerModule(new JavaTimeModule());  // Register JavaTimeModule with ObjectMapper
-
+        mapper.registerModule(new JavaTimeModule());
         JsonNode node = mapper.readTree(p);
 
         // Log the entire node structure
         System.out.println("JSON Node: " + node.toString());
 
+        JsonNode contentNode = node.get("content");
+        if (contentNode == null) {
+            throw new JsonProcessingException("Missing 'content' node in JSON") {};
+        }
 
-
-
-        JsonNode beerArray = node.get("content");
         List<BeerDTO> beers = new ArrayList<>();
-        for (JsonNode beerNode : beerArray) {
+        for (JsonNode beerNode : contentNode) {
             BeerDTO beer = mapper.treeToValue(beerNode, BeerDTO.class);
             beers.add(beer);
         }
 
-        // Ensure 'page' node exists before accessing
         JsonNode pageableNode = node.get("pageable");
         if (pageableNode == null) {
             throw new JsonProcessingException("Missing 'pageable' node in JSON") {};
         }
 
-        // Now access the properties from the pageNode
-
         int page = pageableNode.has("pageNumber") && !pageableNode.get("pageNumber").isNull() ? pageableNode.get("pageNumber").asInt() : 0;
-        int size = pageableNode.has("pageSize") && !pageableNode.get("pageSize").isNull() ? pageableNode.get("pageSize").asInt() : 0;
+        int size = pageableNode.has("pageSize") && !pageableNode.get("pageSize").isNull() ? pageableNode.get("pageSize").asInt() : 1;
         long total = node.has("totalElements") && !node.get("totalElements").isNull() ? node.get("totalElements").asLong() : 0L;
-
 
         // Log the values retrieved
         System.out.println("Page: " + page);
@@ -56,9 +51,5 @@ public class BeerDTOPageDeserializer extends JsonDeserializer<BeerDTOPageImpl<Be
         System.out.println("Total: " + total);
 
         return new BeerDTOPageImpl<>(beers, PageRequest.of(page, size), total);
-//        return null;
-
-
     }
 }
-
