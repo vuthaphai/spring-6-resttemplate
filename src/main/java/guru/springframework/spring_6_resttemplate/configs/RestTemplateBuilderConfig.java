d7b2e137906aa86db1f06f5768f072462442914d
Vutha Phai <vuthaphai@Vuthas-MacBook-Pro.local>
2024-11-17 11:57:17 +0700

    wip

--- src/main/java/guru/springframework/spring_6_resttemplate/configs/RestTemplateBuilderConfig.java
+++ src/main/java/guru/springframework/spring_6_resttemplate/configs/RestTemplateBuilderConfig.java
@@ -13,15 +13,23 @@ public class RestTemplateBuilderConfig {
     @Value("${rest.template.rootUrl}")
     String rootUrl;
 
-    @Bean
-    RestTemplateBuilder restTemplateBuilder(RestTemplateBuilderConfigurer configurer){
+    @Value("${rest.template.username}")
+    String username;
 
-        assert rootUrl !=null;
+    @Value("${rest.template.password}")
+    String password;
+
+    @Bean
+    RestTemplateBuilder restTemplateBuilder(RestTemplateBuilderConfigurer configurer) {
+
+        assert rootUrl != null;
 
         RestTemplateBuilder builder = configurer.configure(new RestTemplateBuilder());
         DefaultUriBuilderFactory uriBuilderFactory = new
                 DefaultUriBuilderFactory(rootUrl);
 
-        return builder.uriTemplateHandler(uriBuilderFactory);
+        RestTemplateBuilder builderWithAuth = builder.basicAuthentication(username, password);
+
+        return builderWithAuth.uriTemplateHandler(uriBuilderFactory);
     }
 }
